250a654aa2eae9b7e72f241cf3c74eb992681009
Paulo André Leal <pauloandeleal@gmail.com>
2025-01-19 17:57:47 +0000

    changed

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/kinex">
+    <BrowserRouter basename="/kinex/dashboard">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
